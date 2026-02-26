96137458ba9659d5899900f700c7827b914b3a65
deel_1003457 <1003457@my.scccd.edu>
2026-02-26 14:04:48 -0800

    forgot to push my header file changes so here they are

--- include/Particle.h
+++ include/Particle.h
@@ -30,15 +30,25 @@ class Particle {
 	Particle();
 	Particle(float r, float c, float xv, float yv, ParticleType t,uint8_t red, uint8_t green, uint8_t blue, bool stat, int life);
 	float getRow() const;
+	void setRow(float r);
 	float getCol() const;
+	void setCol(float c);
 	float getX_Vel() const;
+	void setX_Vel(float xv);
 	float getY_Vel() const;
+	void setY_Vel(float yv);
 	bool get_stationary() const;
+	void set_stationary(bool stat);
 	uint8_t getRed() const;
+	void setRed(uint8_t red);
 	uint8_t getGreen() const;
+	void setGreen(uint8_t green);
 	uint8_t getBlue() const; 
+	void setBlue(uint8_t blue);
 	ParticleType getType() const;
+	void setParticleType(ParticleType t);
 	int getLifetime() const;
+	void setLifetime(int life);
 	
 
 	//core logic stuff, i forward declared world to pass it as a reference so we can cehck surrondings
