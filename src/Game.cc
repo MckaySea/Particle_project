d2696f84a7a26c17b9aadd2158d7e019a2d41954
mouanoutoua_1018227 <1018227@MY.SCCCD.EDU>
2026-02-26 23:37:05 -0800

    finished up adding changes

--- src/Game.cc
+++ src/Game.cc
@@ -60,6 +60,75 @@ void Game::run() {
 			}
 			frame_count++;
 		}
+
+		stringstream ss;
+		ss << "\033[2J\033[1;1H";
+		for(const auto &p : world.getParticles()){
+			int r = (int)p.getRow();
+			int c = (int)p.getCol();
+			if(r >= 0 && r < world.getRows() && c >= 0 && c < world.getCol()){
+				ss << "\033["<< r + 1 << ";" << c + 1 << "H";
+				ss << "\033[48;2;" <<  (int)p.getRed() << ";" << (int)p.getGreen() << ";" << (int)p.getBlue() << 'm';
+				ss << "\033[0m";
+			}
+		}
+
+		ss << "\033[" << world.getRows() + 2 << ";1H";
+		ss << "\033[0m";
+		ss << "Frame: " << frame << " | Alive: " << world.alive_count() << " | Target FPS: " << target fps << " | Status: " <<(is_paused ? "PAUSED" : "RUNNING") << " ";
+		ss << "\033[" << world.getRows() + 3  << ";1H";
+		ss << "Commands: (P)ause | (Q)uit | (C)ycle Element | (L)oad | Sa(v)e(M) |" "(F)aster | (Slower | (D)raw";
+
+		cout << ss.str() << flush;
+
+		auto end_time = chrono::steady_clock::now();
+		auto elapsed = chrono::duration_cast(end_time - start_time).count();
+		int expected_delay = 1000000 / target_fps;
+		if(elapsed < expected_delay){
+			usleep(expected_delay - elapsed);
+		}
+	}
+	
+	set_raw_mode(false);
+	set_cursor_mode(true);
+	set_mouse_mode(false);
+	restcolor();
+	clearscreen();
  }
+
+void Game::render(){
+
+}
+
+void Game draw_bridges(){
+	bridges::Bridges bridges(1, "REPLACE_WITH_USERNAME", "REPLACE_WITH_API_KEY");
+	bridges.setTitles("Particle Simulation");
+
+	int r = world.getRows();
+	int c = world.getCols();
+	if (r <= 0 || c <= 0)
+		return;
+
+	bridges::ColorGrid cg(r, c, bridges::Color("black"));
+
+	for(const auto &p : world.getParticles()){
+		int pr = (int)p.getRow();
+		int pc = (int)p.getCol();
+		if(Pr >= 0 && pr < r && pc >= 0 && pc , c){
+			cg.set(pr, pc, bridges::Color(p.getRed(), p.getGreen(), p.getBlue()));
+		}
+	}
+
+	bridges.setDataStructure(&cg);
+	bridges.visualize();
+}
+
+void Game::start_simulation(){}
+void Game::pause_simulation(){}
+void Game::quit(){}
+void Game::load_world(){}
+void Game::save_world(){}
+void Game::increase_fps(){}
+void Game::decrease_fps(){}
 
 }
