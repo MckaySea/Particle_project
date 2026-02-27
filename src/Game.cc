5623a5d0492a16327462bd4d469b33e8740acc2e
mouanoutoua_1018227 <1018227@MY.SCCCD.EDU>
2026-02-26 21:58:05 -0800

    continuing to add to Game.cc

--- src/Game.cc
+++ src/Game.cc
@@ -2,18 +2,37 @@
 #include "/public/colors.h"
 
 //just initilizing the world with temp dimensions, should be changed
-Game::Game() : world(50, 50) {	
-
+Game::Game() : world(20, 20) {
+	auto [r, c] = get_terminal_size();
+	world = world(r - 5, c);
+	frame = 0;
+	frame_count = 0;
 }
 
 //this is what main.cc is calling and will be our infinite game loop eventually
 void Game::run() {
 	bool is_running = true;
 	bool is_paused = true;
+	int target_fps = 5;
+
 	set_raw_mode(true);
 	set_cursor_mode(false);
+	set_mouse_mode(true);
+
+	static ParticleType current_spawn_type = ParticleType::WATER;
+
+	on_mousedown(mousedown_handler);
+
+
 	clearscreen();
+	movecursor(world.getRows() / 2, world.getCols() / 2 - 15);
+	cout << "Welcome to Particle Sim! Press any key to start.";
+	while(quick_read() == ERR){
+		usleep(10000);
+	}
+
 	while(is_running){
+		auto start_time = chrono::steady_clock::now();
 		int input = quick_read();
 		if(input == 'q' || input == 'Q'){
 			is_running = false;
@@ -23,13 +42,23 @@ void Game::run() {
 			frame_count++;
 		} else if (input == 's' || input == 'S'){ //slow down frame rate
 			frame_count--;
+			if(target_fps < 1)
+				target_fps = 1;
 		} else if (input == 'l' || input == 'L'){ // load the world from disk
-
-		} else if (input == 'm' || input == 'M'){ // save the world to disk
-
+			world.load("world_save.txt");
+		} else if (input == 'm' || input == 'M' || input == 'w' || input == 'W'){ // save to disk
+			world.save("world_save.txt");
+		} else if (input == 'd' || input 'D'){
+			draw_bridges();
 		}
+
 		if(!is_paused){
-		frame_count++;
+			if(world.alive_count() == 0){
+				is_paused = true;
+			} else {
+				world.physics();
+			}
+			frame_count++;
 		}
  }
 
